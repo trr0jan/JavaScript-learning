f372f1665f87ad15002059d7d670b658ae11069d
trr0jan <nesterchukmaxim09@gmail.com>
2024-06-09 11:57:42 +0300

    adding steps and random numbers

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
-let country;
-let city;
-let adress;
-let fullAdress = "Ukraine, Mykolaiv, Karpenko str";
+let country = 'Ukraine';
+let city = 'Mykolaiv';
+let adress = 'Karpenko str';
+let fullAdress = country + city + adress;
 
 let sideA = 17;
 let sideB = 21;
 let area = sideA * sideB;
 
 console.log('Area = ' + area + 'm2')
+
+let number1 = 17;
+let number2 = 6;
+
+console.log(number1 ** number2)
+
+console.log(Math.floor(Math.random() * 70))
