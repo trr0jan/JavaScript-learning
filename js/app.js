ca7ecd94e2532c4caeb9cdb5beb000785f5a5795
trr0jan <nesterchukmaxim09@gmail.com>
2024-06-09 11:38:56 +0300

    adding an area calculator

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let country;
+let city;
+let adress;
+let fullAdress = "Ukraine, Mykolaiv, Karpenko str";
+
+let sideA = 17;
+let sideB = 21;
+let area = sideA * sideB;
+
+console.log('Area = ' + area + 'm2')
